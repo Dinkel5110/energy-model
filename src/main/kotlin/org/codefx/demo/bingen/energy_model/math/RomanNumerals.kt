d5654cf885fb28ded81ce8d4fdbbed753e8af692
Steffen Konrad <steffen.konrad@th-bingen.de>
2017-07-01 20:43:23 +0200

    Implement the map idea

--- src/main/kotlin/org/codefx/demo/bingen/energy_model/math/RomanNumerals.kt
+++ src/main/kotlin/org/codefx/demo/bingen/energy_model/math/RomanNumerals.kt
@@ -1,7 +1,16 @@
 package org.codefx.demo.bingen.energy_model.math
 
-fun asRoman(n: Int): String {
-    return ""
-}
+val decimalToRomanMapping : Map<Int, String> = mapOf(
 
+        Pair(50, "L"),
+        Pair(40, "XL"),
+        Pair(10, "X"),
+        Pair(9, "IX"),
+        Pair(5, "V"),
+        Pair(4, "IV"),
+        Pair(1, "I")
+)
+
+
+fun asRoman(n: Int): String {
 
