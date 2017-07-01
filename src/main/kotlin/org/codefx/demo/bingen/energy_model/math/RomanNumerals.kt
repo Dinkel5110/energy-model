e482ae29b735b4cf880a13d7e5f5015906ede9b3
Steffen Konrad <steffen.konrad@th-bingen.de>
2017-07-01 22:53:21 +0200

    Roman Numerals Tests sloved

--- src/main/kotlin/org/codefx/demo/bingen/energy_model/math/RomanNumerals.kt
+++ src/main/kotlin/org/codefx/demo/bingen/energy_model/math/RomanNumerals.kt
@@ -2,7 +2,7 @@ package org.codefx.demo.bingen.energy_model.math
 
 val decimalToRomanMapping : Map<Int, String> = mapOf(
 
-        Pair(50, "L"),
+        Pair(50, "L"),      //Mapping of the values needed to compile the Roman numerals
         Pair(40, "XL"),
         Pair(10, "X"),
         Pair(9, "IX"),
@@ -12,5 +12,21 @@ val decimalToRomanMapping : Map<Int, String> = mapOf(
 )
 
 
-fun asRoman(n: Int): String {
+fun asRoman(n: Int): String {          //the function
 
+    var decimalRemainder = n           // Declaration of the variable
+    var romanOutput = ""               // "" for the correct output
+
+
+    decimalToRomanMapping.forEach {        //
+
+        val (decimal, roman) = it
+
+
+        while(decimalRemainder >= decimal) {
+            romanOutput += roman
+            decimalRemainder -= decimal
+        }
+    }
+    return romanOutput
+}
